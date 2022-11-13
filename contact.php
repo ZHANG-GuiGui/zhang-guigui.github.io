6f43c7580cef0a37513a7853899ec8015c5acc9b
unknown <txxbb@outlook.com>
2022-11-13 16:08:29 +0800

    contact

--- contact.php
+++ contact.php
@@ -2,11 +2,11 @@
 if (isset($_POST['email']) && !empty($_POST['email'])) {
   $subject = "New contact request: $_POST['subject']";
   $message = $_POST['message'];
-  $headers = 'From: info@website.com' . "\r\n" .
+  $headers = 'From: my@website.com' . "\r\n" .
              'Reply-To: ' . $_POST['email']. "\r\n" .
              'X-Mailer: PHP/' . phpversion();
 
-  mail('info@example.com', $subject, $message, $headers);
+  mail('txxbb@outlook.com', $subject, $message, $headers);
 
   die('Thank you for your email');
 }
